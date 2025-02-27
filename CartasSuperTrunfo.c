816255dee459b021e801bc73aab7d334854bc50d
gabriel8878 <yangabriel.office@gmail.com>
2025-02-27 23:24:01 +0000

    Incluido super poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -34,39 +34,27 @@ void cadastrarCidades(struct Cidade cidades[NUM_ESTADOS][CIDADES_POR_ESTADO]) {
     }
 }
 
+float calcularSuperPoder(struct Cidade c) {
+    float densidade = c.area > 0 ? c.populacao / c.area : 0;
+    float pib_per_capita = c.populacao > 0 ? c.pib / c.populacao : 0;
+    return c.populacao + c.pib + pib_per_capita + (1 / (densidade + 1)) + c.pontos_turisticos;
+}
+
 void compararCartas(struct Cidade c1, struct Cidade c2) {
-    int pontos_c1 = 0, pontos_c2 = 0;
+    float poder_c1 = calcularSuperPoder(c1);
+    float poder_c2 = calcularSuperPoder(c2);
 
     printf("\nComparacao de Cartas:\n\n");
-    printf("Populacao: Carta %d venceu (%d)\n", c1.populacao > c2.populacao ? 1 : 2, c1.populacao > c2.populacao);
-    pontos_c1 += c1.populacao > c2.populacao;
-    pontos_c2 += c2.populacao > c1.populacao;
+    printf("Super Poder Total - Carta 1: %.2f\n", poder_c1);
+    printf("Super Poder Total - Carta 2: %.2f\n", poder_c2);
 
-    printf("Area: Carta %d venceu (%d)\n", c1.area > c2.area ? 1 : 2, c1.area > c2.area);
-    pontos_c1 += c1.area > c2.area;
-    pontos_c2 += c2.area > c1.area;
-
-    printf("PIB: Carta %d venceu (%d)\n", c1.pib > c2.pib ? 1 : 2, c1.pib > c2.pib);
-    pontos_c1 += c1.pib > c2.pib;
-    pontos_c2 += c2.pib > c1.pib;
-
-    printf("Pontos Turisticos: Carta %d venceu (%d)\n", c1.pontos_turisticos > c2.pontos_turisticos ? 1 : 2, c1.pontos_turisticos > c2.pontos_turisticos);
-    pontos_c1 += c1.pontos_turisticos > c2.pontos_turisticos;
-    pontos_c2 += c2.pontos_turisticos > c1.pontos_turisticos;
-
-    float densidade_c1 = c1.area > 0 ? c1.populacao / c1.area : 0;
-    float densidade_c2 = c2.area > 0 ? c2.populacao / c2.area : 0;
-    printf("Densidade Populacional: Carta %d venceu (%d)\n", densidade_c1 > densidade_c2 ? 1 : 2, densidade_c1 > densidade_c2);
-    pontos_c1 += densidade_c1 > densidade_c2;
-    pontos_c2 += densidade_c2 > densidade_c1;
-
-    float pib_per_capita_c1 = c1.populacao > 0 ? c1.pib / c1.populacao : 0;
-    float pib_per_capita_c2 = c2.populacao > 0 ? c2.pib / c2.populacao : 0;
-    printf("PIB per Capita: Carta %d venceu (%d)\n", pib_per_capita_c1 > pib_per_capita_c2 ? 1 : 2, pib_per_capita_c1 > pib_per_capita_c2);
-    pontos_c1 += pib_per_capita_c1 > pib_per_capita_c2;
-    pontos_c2 += pib_per_capita_c2 > pib_per_capita_c1;
-
-    printf("Super Poder: Carta %d venceu (%d)\n", pontos_c1 > pontos_c2 ? 1 : 2, pontos_c1 > pontos_c2);
+    if (poder_c1 > poder_c2) {
+        printf("Carta 1 venceu!\n");
+    } else if (poder_c2 > poder_c1) {
+        printf("Carta 2 venceu!\n");
+    } else {
+        printf("Empate!\n");
+    }
 }
 
 int main() {
@@ -85,3 +73,4 @@ int main() {
     
     return 0;
 }
+
